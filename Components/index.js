f0706fe245288390f0bf4fa6304d8159f26a0fb8
Leonardo Tapia <ltapia@iMac.local>
2020-07-28 14:23:36 -0500

    First commit

---
+++ Components/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Image } from 'react-native'
+
+
+function index(props){
+    
+    let styles = {
+        width: 100,
+        height: 100,
+        resizeMode: 'cover',
+    }
+
+    if(props.tiny){
+        styles.width= 50
+        styles.height= 50
+    }
+
+    if(props.small){
+        styles.width= 70
+        styles.height= 70
+    }
+
+
+    if(props.fullRound){
+        styles = {
+            ...styles,
+            borderRadius : 200,
+            }
+    }
+
+    if(props.round){
+        styles = {
+            ...styles,
+            borderRadius : props.round,
+            }
+    }
+
+
+    let onPressHandler =  ()=>{}
+
+    if(props.onPressHandler)
+        onPressHandler = props.onPressHandler
+
+
+    return ( <Image
+        style={styles}
+        source={{
+        uri : props.uri
+    }}/> )
+}
+
+
+export default index
+
